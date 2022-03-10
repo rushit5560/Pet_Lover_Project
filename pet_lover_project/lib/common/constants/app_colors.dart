956994757dc4c7db64a57889b15627a24a7185b3
jyoti <90950176+jyoti5560@users.noreply.github.com>
2022-03-10 11:53:45 +0530

    Add color

--- pet_lover_project/lib/common/constants/app_colors.dart
+++ pet_lover_project/lib/common/constants/app_colors.dart
@@ -1,1 +1,8 @@
-class AppColors {}
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color colorDarkBlue1 = const Color(0xFF4E83A2);
+  static Color colorDarkBlue = const Color(0xFF507295);
+  static Color colorLightBlue1 = const Color(0xFF68B5E1);
+  static Color colorLightBlue = const Color(0xFFEDF3F6);
+}
