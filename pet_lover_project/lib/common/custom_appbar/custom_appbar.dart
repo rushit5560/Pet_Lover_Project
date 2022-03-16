9297261c9350f2aabf968615e946cb9d1496ec23
Rushit <rushit@omtecweb.com>
2022-03-16 11:22:00 +0530

    AppBar Moduke Change

--- pet_lover_project/lib/common/custom_appbar/custom_appbar.dart
+++ pet_lover_project/lib/common/custom_appbar/custom_appbar.dart
@@ -6,42 +6,18 @@ import '../constants/enums.dart';
 class CommonAppBarModule extends StatelessWidget {
   final String title;
   final AppBarOption appBarOption;
+  Function? onTap;
 
   CommonAppBarModule(
-      {Key? key, required this.title, required this.appBarOption})
+      {Key? key, required this.title, required this.appBarOption, this.onTap})
       : super(key: key);
-
-  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Material(
-          elevation: 10,
-          borderRadius: BorderRadius.circular(10),
-          child: GestureDetector(
-            onTap: () {
-              if (appBarOption == AppBarOption.homeScreenOption) {
-                // scaffoldKey.currentState!.openDrawer();
-                Scaffold.of(context).openDrawer();
-              }
-            },
-            child: Container(
-              height: 52,
-              width: 52,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10), color: Colors.white),
-              child: Image.asset(
-                appBarOption == AppBarOption.homeScreenOption
-                    ? AppImages.menuIconImg
-                    : AppImages.backButtonImg,
-                scale: 2,
-              ),
-            ),
-          ),
-        ),
+        _leftSideButton(context),
         appBarOption == AppBarOption.homeScreenOption
             ? Image.asset(AppImages.petLoverNameImg, scale: 3)
             : Text(
@@ -51,7 +27,8 @@ class CommonAppBarModule extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                     fontSize: 22),
               ),
-        appBarOption == AppBarOption.homeScreenOption
+        _rightSideButton(),
+        /*appBarOption == AppBarOption.homeScreenOption
             ? Material(
                 elevation: 10,
                 borderRadius: BorderRadius.circular(10),
@@ -61,10 +38,7 @@ class CommonAppBarModule extends StatelessWidget {
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10),
                       color: Colors.white),
-                  child: Image.asset(
-                    AppImages.msgIconImg,
-                    scale: 2,
-                  ),
+                  child: Image.asset(AppImages.msgIconImg, scale: 2),
                 ),
               )
             : appBarOption == AppBarOption.petShopScreenOption
@@ -93,10 +67,7 @@ class CommonAppBarModule extends StatelessWidget {
                           decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(10),
                               color: Colors.white),
-                          child: Image.asset(
-                            AppImages.threeDotImg,
-                            scale: 2,
-                          ),
+                          child: Image.asset(AppImages.threeDotImg, scale: 2),
                         ),
                       )
                     : appBarOption == AppBarOption.petShopScreenOption
@@ -109,18 +80,63 @@ class CommonAppBarModule extends StatelessWidget {
                               decoration: BoxDecoration(
                                   borderRadius: BorderRadius.circular(10),
                                   color: Colors.white),
-                              child: Image.asset(
-                                AppImages.profilePicImg,
-                                scale: 2,
-                              ),
+                              child: Image.asset(AppImages.profilePicImg, scale: 2),
                             ),
                           )
                         : Container(
-                            height: 45,
-                            width: 45,
+                            height: 52,
+                            width: 52,
                             color: Colors.transparent,
-                          ),
+                          ),*/
       ],
+    );
+  }
+
+  Widget _leftSideButton(BuildContext context) {
+    return Material(
+      elevation: 10,
+      borderRadius: BorderRadius.circular(10),
+      child: GestureDetector(
+        onTap: () {
+          if (appBarOption == AppBarOption.homeScreenOption) {
+            Scaffold.of(context).openDrawer();
+          }
+        },
+        child: Container(
+          height: 52,
+          width: 52,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10), color: Colors.white),
+          child: Image.asset(
+            appBarOption == AppBarOption.homeScreenOption
+                ? AppImages.menuIconImg
+                : AppImages.backButtonImg,
+            scale: 2,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _rightSideButton() {
+    return Material(
+      elevation: 10,
+      borderRadius: BorderRadius.circular(10),
+      child: Container(
+        height: 52,
+        width: 52,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10), color: Colors.white),
+        child: appBarOption == AppBarOption.homeScreenOption
+          ? Image.asset(AppImages.msgIconImg, scale: 2)
+        : appBarOption == AppBarOption.petShopScreenOption
+        ? Image.asset(AppImages.cartImg, scale: 2)
+        : appBarOption == AppBarOption.userProfileScreenOption
+        ? Image.asset(AppImages.threeDotImg, scale: 2)
+        : appBarOption == AppBarOption.petViewScreenOption
+        ? Image.asset(AppImages.profilePicImg, scale: 2)
+        : null,
+      ),
     );
   }
 }
